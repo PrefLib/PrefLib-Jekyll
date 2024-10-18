f5e2bec7131ba732ed51e1c736d779dfcbcea9e5
Simon Rey <reysimon@orange.fr>
2024-10-18 16:37:18 +0200

    Alpha version ready

--- scripts/import_dataset.py
+++ scripts/import_dataset.py
@@ -1,11 +1,10 @@
+import csv
 import os
 import shutil
 from zipfile import ZipFile
 
 import yaml
-from preflibtools.instances import get_parsed_instance
 from preflibtools.instances.dataset import read_info_file
-from preflibtools.properties.basic import is_approval
 
 zip_files = sorted([os.path.join("zip", f) for f in os.listdir("zip") if f.endswith(".zip")])
 # zip_files = ["zip/00001 - irish.zip", "zip/00002 - debian.zip", "zip/00003 - nasa.zip"]
@@ -37,31 +36,39 @@ for zip_file in zip_files:
         "selected_studies": dataset_info["studies"],
         "num_files": len(dataset_info["files"]),
         "zip_file_size": os.path.getsize(zip_file),
-        "zip_file_url": f"https://github.com/PrefLib/PrefLib-Data/raw/refs/heads/main/zip/dataset/{dataset_info['series']} - {dataset_info['abb']}.zip",
+        "zip_file_url": f"https://github.com/PrefLib/PrefLib-Data/releases/download/v1.0/{dataset_info['series']}_{dataset_info['abb']}.zip",
         "data_types": list(set(f["file_name"][-3:] for f in dataset_info["files"].values()))
     }
     with open(dataset_file_path, "a", encoding="utf-8") as dataset_file:
             yaml.dump([dataset_yml], dataset_file, default_flow_style=False, sort_keys=False, encoding='utf-8', allow_unicode=True)
 
+    file_to_metadatas = dict()
+    with open(os.path.join(tmp_dir_path, "metadata.csv"), encoding="utf-8") as f:
+        reader = csv.DictReader(f, delimiter=";")
+        for row in reader:
+            file_to_metadatas[row["file"]] = row
+
     datafile_yml = dict()
     for file_name, file_info in dataset_info["files"].items():
         print(f"\t{file_name}")
         if file_name[-3:] in ["toc", "toi", "soc", "soi", "cat", "wmd"]:
-            instance = get_parsed_instance(os.path.join(tmp_dir_path, file_name))
-            num_voters = instance.num_voters
-            num_alternatives = instance.num_alternatives
-            approval_instance = is_approval(instance) if instance.data_type in ["toc", "soc", "toi", "soi", "cat"] else None
-            if instance.data_type in ["soc", "soi", "toc", "toi"]:
-                num_unique_preferences = instance.num_unique_orders
-            elif instance.data_type == "cat":
-                num_unique_preferences = instance.num_unique_preferences
-            else:
-                num_unique_preferences = 0
+            metadata = file_to_metadatas[file_name]
         else:
+            metadata = dict()
+
+        try:
+            num_voters = int(metadata["numVot"])
+        except:
             num_voters = 0
+        try:
+            num_alternatives = int(metadata["numAlt"])
+        except:
             num_alternatives = 0
-            num_unique_preferences = 0
-            approval_instance = None
+        try:
+            num_unique = int(metadata["numUniq"])
+        except:
+            num_unique = 0
+
         datafile_yml[file_name] = {
             "name": file_name,
             "dataset": dataset_info["abb"],
@@ -76,8 +83,8 @@ for zip_file in zip_files:
             "url": f"https://raw.githubusercontent.com/PrefLib/PrefLib-Data/main/datasets/{dataset_info['series']} - {dataset_info['abb']}/{file_name}",
             "num_voters": num_voters,
             "num_alternatives": num_alternatives,
-            "num_unique_preferences": num_unique_preferences,
-            "is_approval": approval_instance,
+            "num_unique_preferences": num_unique,
+            "is_approval": metadata.get("isApproval", None) == "True",
             "is_single_peaked": None,
             "is_single_peaked_circle": None,
             "is_single_peaked_tree": None,
