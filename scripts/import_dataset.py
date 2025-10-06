5747e8dd65aa2a690d51529829de3d6b57414941
Simon-Rey <simon.j.rey@proton.me>
2025-10-06 17:32:25 +0200

    Polis Dataset

--- scripts/import_dataset.py
+++ scripts/import_dataset.py
@@ -6,105 +6,106 @@ from zipfile import ZipFile
 import yaml
 from preflibtools.instances.dataset import read_info_file
 
-zip_files = sorted([os.path.join("zip", f) for f in os.listdir("zip") if f.endswith(".zip")])
-# zip_files = ["zip/00001 - irish.zip", "zip/00002 - debian.zip", "zip/00003 - nasa.zip"]
+if __name__ == "__main__":
+    zip_files = sorted([os.path.join("zip", f) for f in os.listdir("zip") if f.endswith(".zip")])
+    # zip_files = ["zip/00001 - irish.zip", "zip/00002 - debian.zip", "zip/00003 - nasa.zip"]
 
-dataset_file_path = os.path.join("..", "preflib", "_data", "datasets.yml")
-datafile_file_path = os.path.join("..", "preflib", "_data", "datafiles.yml")
+    dataset_file_path = os.path.join("..", "preflib", "_data", "datasets.yml")
+    datafile_file_path = os.path.join("..", "preflib", "_data", "datafiles.yml")
 
-with open(dataset_file_path, "w", encoding="utf-8") as dataset_file:
-    pass
-with open(datafile_file_path, "w", encoding="utf-8") as datafile_file:
-    pass
+    with open(dataset_file_path, "w", encoding="utf-8") as dataset_file:
+        pass
+    with open(datafile_file_path, "w", encoding="utf-8") as datafile_file:
+        pass
 
-for zip_file in zip_files:
-    print(f"Importing {zip_file}")
-    tmp_dir_path = "tmp"
-    os.makedirs(tmp_dir_path, exist_ok=True)
+    for zip_file in zip_files:
+        print(f"Importing {zip_file}")
+        tmp_dir_path = "tmp"
+        os.makedirs(tmp_dir_path, exist_ok=True)
 
-    with ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall(tmp_dir_path)
-    dataset_info = read_info_file(os.path.join(tmp_dir_path, "info.txt"))
-    dataset_yml = {
-        "name": dataset_info["name"],
-        "abbreviation": dataset_info["abb"],
-        "series_number": dataset_info["series"],
-        "tags": dataset_info["tags"],
-        "publication_date": dataset_info["publication_date"],
-        "description": dataset_info["description"],
-        "required_citations": dataset_info["citations"],
-        "selected_studies": dataset_info["studies"],
-        "num_files": len(dataset_info["files"]),
-        "zip_file_size": os.path.getsize(zip_file),
-        "zip_file_url": f"https://github.com/PrefLib/PrefLib-Data/releases/download/v1.0/{dataset_info['series']}_{dataset_info['abb']}.zip",
-        "data_types": list(set(f["file_name"][-3:] for f in dataset_info["files"].values()))
-    }
-    with open(dataset_file_path, "a", encoding="utf-8") as dataset_file:
-            yaml.dump([dataset_yml], dataset_file, default_flow_style=False, sort_keys=False, encoding='utf-8', allow_unicode=True)
-
-    file_to_metadatas = dict()
-    with open(os.path.join(tmp_dir_path, "metadata.csv"), encoding="utf-8") as f:
-        reader = csv.DictReader(f, delimiter=";")
-        for row in reader:
-            file_to_metadatas[row["file"]] = row
-
-    datafile_yml = dict()
-    for file_name, file_info in dataset_info["files"].items():
-        print(f"\t{file_name}")
-        if file_name[-3:] in ["toc", "toi", "soc", "soi", "cat", "wmd"]:
-            metadata = file_to_metadatas[file_name]
-        else:
-            metadata = dict()
-
-        try:
-            num_voters = int(metadata["numVot"])
-        except:
-            num_voters = 0
-        try:
-            num_alternatives = int(metadata["numAlt"])
-        except:
-            num_alternatives = 0
-        try:
-            num_unique = int(metadata["numUniq"])
-        except:
-            num_unique = 0
-
-        datafile_yml[file_name] = {
-            "name": file_name,
-            "dataset": dataset_info["abb"],
-            "data_type": file_name[-3:],
-            "modification_type": file_info["modification_type"],
-            "relates_to": file_info["relates_to"],
-            "related_files": [],
-            "title": file_info["title"],
-            "description": file_info["description"],
-            "publication_date": file_info["publication_date"],
-            "size": os.path.getsize(os.path.join(tmp_dir_path, file_name)),
-            "url": f"https://raw.githubusercontent.com/PrefLib/PrefLib-Data/main/datasets/{dataset_info['series']} - {dataset_info['abb']}/{file_name}",
-            "num_voters": num_voters,
-            "num_alternatives": num_alternatives,
-            "num_unique_preferences": num_unique,
-            "is_approval": metadata.get("isApproval", None) == "True",
-            "is_strict": metadata.get("isStrict", None) == "True",
-            "is_complete": metadata.get("isComplete", None) == "True",
-            "is_single_peaked": metadata.get("isSP", None) == "True",
-            "is_single_peaked_tree": metadata.get("isSPTree", None) == "True",
-            "is_single_crossing": metadata.get("isSC", None) == "True",
+        with ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(tmp_dir_path)
+        dataset_info = read_info_file(os.path.join(tmp_dir_path, "info.txt"))
+        dataset_yml = {
+            "name": dataset_info["name"],
+            "abbreviation": dataset_info["abb"],
+            "series_number": dataset_info["series"],
+            "tags": dataset_info["tags"],
+            "publication_date": dataset_info["publication_date"],
+            "description": dataset_info["description"],
+            "required_citations": dataset_info["citations"],
+            "selected_studies": dataset_info["studies"],
+            "num_files": len(dataset_info["files"]),
+            "zip_file_size": os.path.getsize(zip_file),
+            "zip_file_url": f"https://github.com/PrefLib/PrefLib-Data/releases/download/v1.0/{dataset_info['series']}_{dataset_info['abb']}.zip",
+            "data_types": list(set(f["file_name"][-3:] for f in dataset_info["files"].values()))
         }
-    files_to_remove = []
-    for file_name, file_dict in datafile_yml.items():
-        if file_dict["relates_to"]:
-            datafile_yml[file_dict["relates_to"]]["related_files"].append({
-                "name": file_dict["name"],
-                "description": file_dict["description"],
-                "url": file_dict["url"],
-                "size": file_dict["size"]
-                })
-            files_to_remove.append(file_name)
-    for f in files_to_remove:
-        del datafile_yml[f]
+        with open(dataset_file_path, "a", encoding="utf-8") as dataset_file:
+                yaml.dump([dataset_yml], dataset_file, default_flow_style=False, sort_keys=False, encoding='utf-8', allow_unicode=True)
 
-    with open(datafile_file_path, "a", encoding="utf-8") as datafile_file:
-            yaml.dump(list(datafile_yml.values()), datafile_file, default_flow_style=False, sort_keys=False, encoding='utf-8', allow_unicode=True)
+        file_to_metadatas = dict()
+        with open(os.path.join(tmp_dir_path, "metadata.csv"), encoding="utf-8") as f:
+            reader = csv.DictReader(f, delimiter=";")
+            for row in reader:
+                file_to_metadatas[row["file"]] = row
 
-    shutil.rmtree(tmp_dir_path)
+        datafile_yml = dict()
+        for file_name, file_info in dataset_info["files"].items():
+            print(f"\t{file_name}")
+            if file_name[-3:] in ["toc", "toi", "soc", "soi", "cat", "wmd"]:
+                metadata = file_to_metadatas[file_name]
+            else:
+                metadata = dict()
+
+            try:
+                num_voters = int(metadata["numVot"])
+            except:
+                num_voters = 0
+            try:
+                num_alternatives = int(metadata["numAlt"])
+            except:
+                num_alternatives = 0
+            try:
+                num_unique = int(metadata["numUniq"])
+            except:
+                num_unique = 0
+
+            datafile_yml[file_name] = {
+                "name": file_name,
+                "dataset": dataset_info["abb"],
+                "data_type": file_name[-3:],
+                "modification_type": file_info["modification_type"],
+                "relates_to": file_info["relates_to"],
+                "related_files": [],
+                "title": file_info["title"],
+                "description": file_info["description"],
+                "publication_date": file_info["publication_date"],
+                "size": os.path.getsize(os.path.join(tmp_dir_path, file_name)),
+                "url": f"https://raw.githubusercontent.com/PrefLib/PrefLib-Data/main/datasets/{dataset_info['series']} - {dataset_info['abb']}/{file_name}",
+                "num_voters": num_voters,
+                "num_alternatives": num_alternatives,
+                "num_unique_preferences": num_unique,
+                "is_approval": metadata.get("isApproval", None) == "True",
+                "is_strict": metadata.get("isStrict", None) == "True",
+                "is_complete": metadata.get("isComplete", None) == "True",
+                "is_single_peaked": metadata.get("isSP", None) == "True",
+                "is_single_peaked_tree": metadata.get("isSPTree", None) == "True",
+                "is_single_crossing": metadata.get("isSC", None) == "True",
+            }
+        files_to_remove = []
+        for file_name, file_dict in datafile_yml.items():
+            if file_dict["relates_to"]:
+                datafile_yml[file_dict["relates_to"]]["related_files"].append({
+                    "name": file_dict["name"],
+                    "description": file_dict["description"],
+                    "url": file_dict["url"],
+                    "size": file_dict["size"]
+                    })
+                files_to_remove.append(file_name)
+        for f in files_to_remove:
+            del datafile_yml[f]
+
+        with open(datafile_file_path, "a", encoding="utf-8") as datafile_file:
+                yaml.dump(list(datafile_yml.values()), datafile_file, default_flow_style=False, sort_keys=False, encoding='utf-8', allow_unicode=True)
+
+        shutil.rmtree(tmp_dir_path)
--- scripts/test_broken_links.py
+++ scripts/test_broken_links.py
@@ -3,49 +3,49 @@ import warnings
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
-import time
 
-ALREADY_CHECKED_LINKS = set()
-
-
-def check_link(url):
-    try:
-        response = requests.head(url, allow_redirects=True, timeout=5)
-        if response.status_code >= 400:
-            warnings.warn(f"Broken link: {url} - Status code: {response.status_code}")
-        else:
-            pass
-            # print(f"OK link: {url}")
-        return response.status_code
-    except requests.RequestException as e:
-        warnings.warn(f"Error checking link: {url} - {e}")
-        return None
+if __name__ == "__main__":
+    ALREADY_CHECKED_LINKS = set()
 
 
-def crawl_and_check(base_url, max_depth=2, current_depth=0):
-    print(f"Opening: {base_url}")
-    if max_depth is not None and current_depth > max_depth:
-        return
-
-    try:
-        response = requests.get(base_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        links = [urljoin(base_url, link.get('href')) for link in soup.find_all('a', href=True)]
-
-        for link in links:
-            # Only check internal links
-            if urlparse(link).netloc == urlparse(base_url).netloc and link not in ALREADY_CHECKED_LINKS:
-                ALREADY_CHECKED_LINKS.add(link)
-                check_link(link)
-                # Recursive crawl
-                crawl_and_check(link, max_depth, current_depth + 1)
-            elif link not in ALREADY_CHECKED_LINKS:
-                # For external links, just check status without recursion
-                ALREADY_CHECKED_LINKS.add(link)
-                check_link(link)
-    except requests.RequestException as e:
-        print(f"Error retrieving page: {base_url} - {e}")
+    def check_link(url):
+        try:
+            response = requests.head(url, allow_redirects=True, timeout=5)
+            if response.status_code >= 400:
+                warnings.warn(f"Broken link: {url} - Status code: {response.status_code}")
+            else:
+                pass
+                # print(f"OK link: {url}")
+            return response.status_code
+        except requests.RequestException as e:
+            warnings.warn(f"Error checking link: {url} - {e}")
+            return None
 
 
-crawl_and_check('https://preflib.github.io/PrefLib-Jekyll/', max_depth=None)
+    def crawl_and_check(base_url, max_depth=2, current_depth=0):
+        print(f"Opening: {base_url}")
+        if max_depth is not None and current_depth > max_depth:
+            return
+
+        try:
+            response = requests.get(base_url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            links = [urljoin(base_url, link.get('href')) for link in soup.find_all('a', href=True)]
+
+            for link in links:
+                # Only check internal links
+                if urlparse(link).netloc == urlparse(base_url).netloc and link not in ALREADY_CHECKED_LINKS:
+                    ALREADY_CHECKED_LINKS.add(link)
+                    check_link(link)
+                    # Recursive crawl
+                    crawl_and_check(link, max_depth, current_depth + 1)
+                elif link not in ALREADY_CHECKED_LINKS:
+                    # For external links, just check status without recursion
+                    ALREADY_CHECKED_LINKS.add(link)
+                    check_link(link)
+        except requests.RequestException as e:
+            print(f"Error retrieving page: {base_url} - {e}")
+
+
+    crawl_and_check('https://preflib.github.io/PrefLib-Jekyll/', max_depth=None)
